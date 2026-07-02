e7baef96361a84d34bd0a7ee0e7c6ff9fe5d2cf9
M Tanzim Rahman <tanzimr59@gmail.com>
2026-07-02 16:10:54 +0000

    20% lab_8

---
+++ week_9/lab_8/DLL.h
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Node{
+private:
+    int data;
+    Node* prev;
+    Node* next;
+public:
+    Node(int value);
+    Node(int value, Node* after, Node* before);
+    friend class DLL;
+};
+class DLL{
+private: 
+    Node* head;
+    Node* tail;
+    int size;
+public:
+    //constructor
+    DLL();
+    //destructor
+    ~DLL();
+    //copy constructor
+    DLL(const DLL &);
+    //copy assignment
+    DLL& operator=(const DLL&);
+    //offset operator
+    int& operator[](const int idx);
+    friend ostream & operator<<(ostream &, const DLL&);
+    //toString method
+    string toString() const;
+    //adds at the begining of the list
+    void addFirst(int data);
+    //adds at the last of the list
+    void addLast(int data);
+    //removes the first element of the list
+    int removeFirst();
+    //removes the last element of the list
+    int removeLast();
+    //inserts data after p
+    void insertAfter(Node* p, int data);
+    //returns the number of elements in the list
+    int size();
+
+    //returns a pointer to the i-th node in the list
+    Node* get(int i);
+};
